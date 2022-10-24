34d5a8e1d63e4a5d4b2722a4ed6641ab43c14bee
Yulianna Shulshenko <yulianna.zhitkovich@mai.ru>
2022-10-24 10:09:44 +0300

    login

---
+++ toys-store/src/components/TextField/textField.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useState } from "react";
+import s from "./textField.module.css";
+
+const TextField = ({ label, type, name, value, onChange, error }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const toggleShowPassword = () => {
+    setShowPassword((prevState) => !prevState);
+  };
+  return (
+    <div className={s.container}>
+      <label htmlFor={name}>{label}</label>
+      <div>
+        <input
+          type={showPassword ? "text" : type}
+          id={name}
+          name={name}
+          value={value}
+          onChange={onChange}
+          className={s.input}
+        />
+        {type === "password" && (
+          <button className={s.btnInput} onClick={toggleShowPassword}>
+            {showPassword ? (
+              <img src="assets/closeEye.png" alt="Скрыть пароль" />
+            ) : (
+              <img src="assets/openEye.png" alt="Показать пароль" />
+            )}
+          </button>
+        )}
+      </div>
+      {error && <p className={s.error}>{error}</p>}
+    </div>
+  );
+};
+
+export default TextField;
--- toys-store/src/pages/Login/loginPage.jsx
+++ toys-store/src/pages/Login/loginPage.jsx
@@ -1,7 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
+import s from "./loginPage.module.css";
+import { useEffect } from "react";
+import TextField from "../../components/TextField/textField";
+import { validator } from "../../utils/validator";
 
 const LoginPage = () => {
-  return <div>тут будет регистрация</div>;
+  const [data, setData] = useState({ email: "", password: "" });
+  const [errors, setErrors] = useState({});
+
+  const handleChange = ({ target }) => {
+    setData((prevState) => ({ ...prevState, [target.name]: target.value }));
+  };
+
+  const validatorConfig = {
+    email: {
+      isRequired: {
+        message: "Электронная почта обязательна для заполнения",
+      },
+      isEmail: {
+        message: "Email введен некорректно",
+      },
+    },
+    password: {
+      isRequired: {
+        message: "Пароль обязателен для заполнения",
+      },
+      isCapitalSymbol: {
+        message: "Пароль должен содержать хотя бы одну заглавную букву",
+      },
+      isContainDigit: {
+        message: "Пароль должен содержать хотя бы одно число",
+      },
+      min: {
+        message: "Пароль должен состоять минимум из 8 символов",
+        value: 8,
+      },
+    },
+  };
+
+  useEffect(() => {
+    validate();
+  }, [data]);
+
+  const validate = () => {
+    const errors = validator(data, validatorConfig);
+    setErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+
+  const isValidButton = Object.keys(errors).length === 0;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const isValid = validate();
+
+    if (!isValid) return;
+
+    console.log(data);
+  };
+
+  return (
+    <div className={s.container}>
+      <div className={s.content}>
+        <h3 className={s.login}>Login</h3>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            label="Электронная почта:"
+            type="text"
+            name="email"
+            value={data.email}
+            onChange={handleChange}
+            error={errors.email}
+          />
+          <TextField
+            label="Пароль:"
+            type="password"
+            name="password"
+            value={data.password}
+            onChange={handleChange}
+            error={errors.password}
+          />
+          <button type="submit" disabled={!isValidButton} className={s.btn}>
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
